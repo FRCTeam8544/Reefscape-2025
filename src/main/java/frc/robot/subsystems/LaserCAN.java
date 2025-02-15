6cf9cb234d40593ab2948fa1d1eec66457564123
TomServo-0x2A <tomservo@galacticfringe.com>
2025-02-15 17:52:31 -0500

    Factor LaserCan to support logging

--- src/main/java/frc/robot/subsystems/LaserCAN.java
+++ src/main/java/frc/robot/subsystems/LaserCAN.java
@@ -2,59 +2,73 @@ package frc.robot.subsystems;
 
 import au.grapplerobotics.ConfigurationFailedException;
 import au.grapplerobotics.LaserCan;
-import edu.wpi.first.wpilibj.TimedRobot;
+import frc.robot.subsystems.LaserCANIO.LaserCANIOInputs;
 
-public class LaserCAN extends TimedRobot {
-  private LaserCan laser;
+public class LaserCAN implements LaserCANIO {
+  private final LaserCan laser;
+  private final String name;
 
-  @Override
-  public void robotInit() {
-    laser = new LaserCan(9);
+  public LaserCAN(String name, int canId, FieldOfView fov) {
+    this.laser = new LaserCan(canId);
+    this.name = name;
+
+    int fovWidth = 0;
+    int fovHeight = 0;
+
+    switch (fov) {
+      case POINT_2_BY_2:
+        fovWidth = 2;
+        fovHeight = 2;
+        break;
+      case NARROW_4_BY_4:
+        fovWidth = 4;
+        fovHeight = 4;
+        break;
+      case MEDIUM_8_BY_8:
+        fovWidth = 8;
+        fovHeight = 8;
+        break;
+      case WIDE_16_BY_16:
+        fovWidth = 16;
+        fovHeight = 16;
+        break;
+    }
+
+    int fovX = fovWidth / 2;
+    int fovY = fovHeight / 2;
+
     // Optionally initialise the settings of the LaserCAN, if you haven't already done so in
     // GrappleHook
     try {
       laser.setRangingMode(LaserCan.RangingMode.SHORT);
-      laser.setRegionOfInterest(new LaserCan.RegionOfInterest(8, 8, 16, 16));
+      laser.setRegionOfInterest(new LaserCan.RegionOfInterest(fovX, fovY, fovWidth, fovHeight));
       laser.setTimingBudget(LaserCan.TimingBudget.TIMING_BUDGET_33MS);
     } catch (ConfigurationFailedException e) {
-      System.out.println("Configuration failed! " + e);
-    }
-  }
-
-  @Override
-  public void robotPeriodic() {
-    LaserCan.Measurement measurement = laser.getMeasurement();
-    if (measurement != null && measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT) {
-      System.out.println("The target is " + measurement.distance_mm + "mm away!");
-    } else {
       System.out.println(
-          "Oh no! The target is out of range, or we can't get a reliable measurement!");
-      // You can still use distance_mm in here, if you're ok tolerating a clamped value or an
-      // unreliable measurement.
+          "LaserCAN(" + String.valueOf(canId) + ") Configuration failed for " + name + "! " + e);
     }
   }
 
-  @Override
-  public void autonomousInit() {}
+  public void updateInputs(LaserCANIOInputs inputs) {
 
-  @Override
-  public void autonomousPeriodic() {}
+    LaserCan.Measurement measurement = laser.getMeasurement();
 
-  @Override
-  public void teleopInit() {}
+    if (measurement != null) {
+      inputs.laserConnected = true;
+      inputs.ambient = measurement.ambient;
+      inputs.status = measurement.status;
+      inputs.distance_mm = measurement.distance_mm;
+    } else {
+      inputs.laserConnected = false;
+      inputs.ambient = 0;
+      inputs.status = LaserCan.LASERCAN_STATUS_OUT_OF_BOUNDS;
+      inputs.distance_mm = 0;
+    }
 
-  @Override
-  public void teleopPeriodic() {}
+    // TODO Add more logic to reject measurements on errors?
+  }
 
-  @Override
-  public void disabledInit() {}
-
-  @Override
-  public void disabledPeriodic() {}
-
-  @Override
-  public void testInit() {}
-
-  @Override
-  public void testPeriodic() {}
+  public String getName() {
+    return name;
+  }
 }
---
+++ src/main/java/frc/robot/subsystems/LaserCANSim.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems;
+
+public class LaserCANSim implements LaserCANIO {
+  private String name;
+
+  public LaserCANSim(String name, int canId) {}
+
+  public void updateInputs(LaserCANIOInputs inputs) {
+    inputs.laserConnected = true;
+    inputs.ambient = 0;
+    inputs.status = 2;
+    inputs.distance_mm = 0;
+  }
+
+  public String getName() {
+    return name + ": LaserCANSim not implemented";
+  }
+}
