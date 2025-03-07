c0d72874967a16b3beb0df6cac9d77c3fdc1c4be
TomServo-0x2A <tomservo@galacticfringe.com>
2025-03-07 16:03:25 -0500

    Clean up climber logic
    Open soft limits and fix units. Enforce soft and hard limits in the periodic function.
    
    Use constants for climb speed.
    
    Add setPoint logging support.

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -8,6 +8,8 @@ import frc.robot.util.LogUtil;
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLimitSwitch;
+import com.revrobotics.spark.config.LimitSwitchConfig.Type;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
@@ -18,47 +20,72 @@ import frc.robot.Constants;
 
 
 public class Climber extends SubsystemBase {
+  private static final double upSoftRotationLimit = 0; // rotations
+  private static final double downSoftRotationLimit = 0.6; // TODO find real values....
+  private static final int motorStallAmpLimit = 10;
+
+  private static final double climberForwardMaxSpeed = 0.2;
+  private static final double climberReverseMaxSpeed = 0.4;
+
   /** Creates a new Climber. */
-  public static SparkMax pusherRight = new SparkMax(Constants.climberConstants.climberCANID, MotorType.kBrushless);
-  public static SparkMax pusherLeft = new SparkMax(Constants.climberConstants.climber2CANID, MotorType.kBrushless);
-  private static SparkMaxConfig rightConfig = new SparkMaxConfig();
-  private static SparkMaxConfig leftConfig = new SparkMaxConfig();
-  private static AbsoluteEncoder encoder = pusherRight.getAbsoluteEncoder();
+  private SparkMax pusherRight = new SparkMax(Constants.climberConstants.climberCANID, MotorType.kBrushless);
+  private SparkMax pusherLeft = new SparkMax(Constants.climberConstants.climber2CANID, MotorType.kBrushless);
+  private SparkMaxConfig rightConfig = new SparkMaxConfig();
+  private SparkMaxConfig leftConfig = new SparkMaxConfig();
+  private AbsoluteEncoder encoder = pusherRight.getAbsoluteEncoder();
 
-  private static final double upSoftRotationLimit = Math.toRadians(170);
-  private static final double downSoftRotationLimit = Math.toRadians(0); 
-  private MotorJointIO climberIO = new MotorJointSparkMax(pusherRight, "Climber", Constants.climberConstants.climberCANID, 
+  // Joint definition and logging
+  private MotorJointIO climberIO = new MotorJointSparkMax(pusherRight,"Climber",
+                                                          Constants.climberConstants.climberCANID, 
                                                           downSoftRotationLimit, upSoftRotationLimit);
-  private static MotorJointIOInputsAutoLogged climberInOutData;
+  private MotorJointIOInputsAutoLogged  climberInOutData = new MotorJointIOInputsAutoLogged();
 
+  private boolean upStopLimitHit = false;
+  private boolean downStopLimitHit = false;
+  
   public Climber() {
-
-    this.climberInOutData = new MotorJointIOInputsAutoLogged();
-
+    
+    // Configure right motor
     rightConfig.idleMode(IdleMode.kBrake);
     rightConfig.inverted(false);
-    rightConfig.smartCurrentLimit(10);
+    rightConfig.smartCurrentLimit(motorStallAmpLimit);
     rightConfig.closedLoop.feedbackSensor(FeedbackSensor.kAbsoluteEncoder);
+
+    // Enable hard limits with directly atatched limit switches
+    rightConfig.limitSwitch
+        .forwardLimitSwitchType(Type.kNormallyOpen)
+        .forwardLimitSwitchEnabled(true)
+        .reverseLimitSwitchType(Type.kNormallyOpen)
+        .reverseLimitSwitchEnabled(true);
+
+    // Apply right motor configuration
     pusherRight.configure(rightConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
     //Follower will mirror the movements of the rightPusher
     leftConfig.follow(Constants.climberConstants.climberCANID,true);
     leftConfig.idleMode(IdleMode.kBrake);
-    leftConfig.smartCurrentLimit(10);
+    leftConfig.smartCurrentLimit(motorStallAmpLimit);
+
+    // Apply right motor configuration
     pusherLeft.configure(leftConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
 
   public void climberClimb(boolean go) {
     if (go && !climberInOutData.upperSoftLimitHit && !climberInOutData.upperLimitHit) {
-      pusherRight.set(.2);} 
-    else {pusherRight.set(0);}
+      setVelocitySetPoint(climberForwardMaxSpeed);
+    } 
+    else {
+      setVelocitySetPoint(0);
+    }
   }
 
   public void climberBack(boolean move){
-    if(move && !climberInOutData.lowerSoftLimitHit && !climberInOutData.lowerLimitHit){ 
-      pusherRight.set(-.2);}
-      else{pusherRight.set(0);}
-
+    if(move && !climberInOutData.lowerSoftLimitHit && !climberInOutData.lowerLimitHit) { 
+      setVelocitySetPoint(climberReverseMaxSpeed);
+    }
+    else {
+      setVelocitySetPoint(0.0);
+    }
   }
 
   @Override
@@ -66,10 +93,37 @@ public class Climber extends SubsystemBase {
     // This method will be called once per scheduler run
     climberIO.updateInputs(climberInOutData);
 
-    // Update commanded outputs
-    // climberInputs.setPointVelocity ....
+    // Check limits
+    downStopLimitHit = climberInOutData.lowerLimitHit || climberInOutData.lowerSoftLimitHit;
+    upStopLimitHit = climberInOutData.upperLimitHit || climberInOutData.upperSoftLimitHit;
 
+    // Apply limits (Postive velocity is assumed to be climber deploy)
+    if (encoder.getVelocity() > 0.0 && downStopLimitHit) {
+      setVelocitySetPoint(0);
+    }
+    // (Negative velocity is assumed to be climber retract)
+    else if (encoder.getVelocity() < 0.0 && upStopLimitHit) {
+      setVelocitySetPoint(0);
+    }
+    
     // Log summary data
     LogUtil.logData("Climber", climberInOutData);
   }
+
+  private void setVelocitySetPoint(double speed) {
+    double limitedSpeed = 0.0;
+    if (speed > 0.0) {
+      climberInOutData.velocitySetPoint = Math.min(speed, climberForwardMaxSpeed);
+    }
+    else {
+      climberInOutData.velocitySetPoint = Math.max(speed, climberReverseMaxSpeed);
+    }
+    pusherRight.set(limitedSpeed);
+  }
+  
+  private void setPositionSetPoint(double position) {
+    climberInOutData.velocitySetPoint = 0.0;
+    climberInOutData.positionSetPoint = 0.0;
+  }
+
 }
